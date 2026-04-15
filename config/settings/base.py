206170f5fdd51028b0d4ed44c376de0793b831d5
gunung-kim <unge001@naver.com>
2026-04-16 08:39:19 +0900

    fix:settings 수정(DB ENGINE 수정)

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "5432"),
         "NAME": os.getenv("DB_NAEM", "django"),
