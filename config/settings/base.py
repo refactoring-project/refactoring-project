13d78b3eba3a20f49e62252c1e3763372eb52a5d
gunung-kim <unge001@naver.com>
2026-04-16 09:07:46 +0900

    fix:ci-setup수정(.env 및 settings.base 수정)

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
